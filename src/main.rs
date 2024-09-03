8d2e0f4695d2926b5c692e2f04a63a440a02de9d
Doug Morris <doug@mhost.com>
2024-09-03 07:00:11 +0200

    stubbing out functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use std::env;
 
 fn main() {
-    // Prints each argument on a separate line
-    for argument in env::args_os() {
-        println!("{argument:?}");
-    }
+    // parse any arguments from the command line
+    parse_command_ilne();
+    // start a thread reading input lines
+    read_input_lines();
+    // start a thread consuming and processing lines, which ouputs the result to the console
+    process_lines();
+}
+
+fn parse_command_ilne() {
+    // parse the command line arguments
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+}
+
+fn read_input_lines() {
+    // read lines from stdin
+    // send lines to the processing thread
+    println!("read_input_lines");
+}
+
+fn process_lines() {
+    // receive lines from the input thread
+    // process the lines
+    // output the result to the console
+    println!("process_lines");
 }

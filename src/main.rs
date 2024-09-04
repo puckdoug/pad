a096714ed48b50fa56d80fd12673feb98b427a2a
Doug Morris <doug@mhost.com>
2024-09-04 08:17:29 +0200

    added channel to allow sender/receiver threads for async processing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use std::env;
 use std::io::IsTerminal;
+use std::sync::mpsc::channel;
+use std::thread;
 
 fn main() {
     // parse any arguments from the command line
     parse_command_line();
+
     // read stdin if there is data to be consumed
     read_stdin();
+
+    // create sending and receiving ends of the channel
+    let (sender, receiver) = channel();
+
     // start a thread reading input lines
-    let line = read_input_lines();
-    // start a thread consuming and processing lines, which ouputs the result to the console
-    process_lines(line);
+    thread::spawn(move || {
+        sender.send(read_input_lines()).unwrap();
+    });
+
+    // start a thread consuming and processing lines, which ouputs
+    // the result to the console
+    while let Ok(to_process) = receiver.recv() {
+        let padded = process_lines(to_process);
+        println!("{padded}");
+    }
 }
 
 fn parse_command_line() {
